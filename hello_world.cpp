0750b1359900c0f0ba9d23c2643cf24300f45691
SilerHofe <sashsa220605@mail.ru>
2025-12-06 16:27:57 +0300

    refactor:mozilla format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
 // Главная функция - точка входа в программу
 int main(){
+
     string name; // Переменная для хранения имени пользователя
 
     // Запрашиваем ввод от пользователя
     cout << "Enter your name: ";
+
     cin >> name; // Читаем ввод пользователя
+
     // Выводим приветственное сообщение
     cout << "Hello world from " << name << endl;
 
